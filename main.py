50fbc8476ae641962fc89e9501086f16f8228832
Borna Barahimi <bornabarahimi@gmail.com>
2023-03-30 15:54:05 -0400

    Implemented the dcrnet model and training process

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import torch
 from torch import nn
+import numpy as np
+
 from dataset import Cost2100DataLoader
+
+from torch.utils.data import DataLoader, TensorDataset
 
 import logging
 import os
+
+from datetime import datetime
+
+from model import DCRNet
+
+from torch.utils.tensorboard import SummaryWriter
 
 from omegaconf import DictConfig, OmegaConf
 import hydra
@@ -16,11 +26,30 @@ def handle_config(cfg):
 	print(OmegaConf.to_yaml(cfg))
 	return cfg
 
+def evaluator(pred, data):
+    with torch.no_grad():
+        # De-centralize
+        data = data - 0.5
+        pred = pred - 0.5
+
+        # Calculate the NMSE
+        power_gt = data[:, 0, :, :] ** 2 + data[:, 1, :, :] ** 2
+        difference = data - pred
+        mse = difference[:, 0, :, :] ** 2 + difference[:, 1, :, :] ** 2
+        nmse = 10 * torch.log10((mse.sum(dim=[1, 2]) / power_gt.sum(dim=[1, 2])).mean())
+        
+        return nmse
+
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg):
 	
 	# Load Config
 	cfg = handle_config(cfg)
+
+	time = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+	# Set up Tensorboard
+	writer = SummaryWriter(f'{cfg.tensorboard_dir}/v{cfg.version}-{time}')
 
 	# Set device
 	if cfg.gpu is None:
@@ -31,29 +60,102 @@ def main(cfg):
 		torch.backends.cudnn.benchmark = True
 	
 	print("Using Device: {}".format(device))
-	raise
 
 	# Load Model
-
-	
-	# Load Pretrained Model
+	model = DCRNet(cfg.db.shape).to(device)
 	
 	# Load Dataset
-	train_loader, val_loader, test_loader = Cost2100DataLoader(
-	root=cfg.db.path,
-	batch_size=cfg.batch_size,
-	num_workers=cfg.num_workers,
-	pin_memory=False,
-	scenario=cfg.db.scenario)()
+	if cfg.db.name == 'small':
+		data = np.load(cfg.db.path)
+		train_loader = DataLoader(TensorDataset(torch.tensor(data, dtype=torch.float32)), batch_size=cfg.batch_size, shuffle=True)
+		val_loader = DataLoader(TensorDataset(torch.tensor(data, dtype=torch.float32)), batch_size=cfg.batch_size, shuffle=True)
+	else:
+		train_loader, val_loader, test_loader = Cost2100DataLoader(
+		root=cfg.db.path,
+		batch_size=cfg.batch_size,
+		num_workers=cfg.num_workers,
+		pin_memory=False,
+		scenario=cfg.db.scenario)()
  
 	# Resume Training
- 
+	# TODO: Implement this
 
 	# Train Model
- 
-	# Evaluate Model
- 
-	# Save Model
+	criterion = nn.MSELoss()
+	optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
+	# scheduler = torch.optim.lr_scheduler.WarmUpCosineAnnealingLR(optimizer=optimizer,
+	# 										 T_max=cfg.epochs * len(train_loader),
+	# 										 T_warmup=30 * len(train_loader),
+	# 										 eta_min=5e-5)
+	scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer,
+														   T_max=cfg.epochs * len(train_loader),
+														   eta_min=5e-5)
+
+	best_nmse = 1e10
+	best_epoch = 0
+	
+	for epoch in range(cfg.epochs):
+		train_loss = train(cfg, writer, epoch, train_loader, model, device, criterion, optimizer, scheduler)
+		if epoch % cfg.val_interval == 0:
+			val_loss, nmse = validate(writer, epoch, val_loader, model, device, criterion)
+
+			if nmse < best_nmse:
+				best_nmse = nmse
+				best_epoch = epoch
+				# Save Model
+				if cfg.save:
+					state = {
+						'epoch': epoch,
+						'state_dict': model.state_dict(),
+						'optimizer': optimizer.state_dict(),
+						'scheduler': scheduler.state_dict(),
+						'best_nmse': best_nmse,
+						'best_epoch': best_epoch
+					}
+					torch.save(state, os.path.join(cfg.checkpoints_dir, f'v{cfg.version}-{time}-best_model.pth'))	
+
+
+def train(cfg, writer, epoch, train_loader, model, device, criterion, optimizer, scheduler):
+	model.train()
+	train_loss = 0
+	num_batches = len(train_loader)
+	for batch_idx, (data,) in enumerate(train_loader):
+		data = data.to(device)
+		optimizer.zero_grad()
+		output = model(data)
+		loss = criterion(output, data)
+		loss.backward()
+		optimizer.step()
+		scheduler.step()
+		train_loss += loss.item()
+		if batch_idx % cfg.log_interval == 0:
+			# Log Epoch, Batch, Loss, LR
+			log.info('Train Epoch: {} [{}/{}]\tLoss: {:.6f}\tLR: {:.6f}'
+         		.format(epoch, batch_idx, num_batches, loss.item(), scheduler.get_last_lr()[0]))
+
+	train_loss /= len(train_loader.dataset)
+	log.info('====> Epoch: {} Average loss: {:.6f}'.format(epoch, train_loss))
+	writer.add_scalar('Train/Loss', train_loss, epoch)
+	writer.add_scalar('Train/LR', scheduler.get_last_lr()[0], epoch)
+	return train_loss
+
+def validate(writer, epoch, val_loader, model, device, criterion):
+	model.eval()
+	val_loss = 0
+	nmse = 0
+	with torch.no_grad():
+		for i, (data,) in enumerate(val_loader):
+			data = data.to(device)
+			output = model(data)
+			val_loss += criterion(output, data).item()
+			nmse += evaluator(output, data).item()
+	val_loss /= len(val_loader.dataset)
+	nmse /= len(val_loader.dataset)
+	writer.add_scalar('Validation/Loss', val_loss, epoch)
+	writer.add_scalar('Validation/NMSE', nmse, epoch)
+	log.info('====> Validation set loss: {:.6f}, NMSE: {:.6f}'.format(val_loss, nmse))
+	
+	return val_loss, nmse
 
 if __name__ == '__main__':
 	main()
---
+++ model.py
@@ -0,0 +1,261 @@
+import torch
+from torch import nn
+import numpy as np
+
+from utils.wrapper import ConvWrapper, LinearWrapper, MaxPoolWrapper
+
+class DConv(nn.Module):
+    def __init__(self, input_shape, dilation):
+        super().__init__()
+        
+        self.input_shape = input_shape
+        
+        conv1 = ConvWrapper(self.input_shape, 2, (3, 1), dilation=dilation)
+        conv2 = ConvWrapper(conv1.output_shape, 2, (1, 3), dilation=dilation)
+        
+        self.dconv = nn.Sequential(conv1,
+                                   nn.BatchNorm2d(conv1.out_channels),
+                                   nn.PReLU(num_parameters=2, init=0.3),
+                                   conv2,
+                                   nn.BatchNorm2d(conv2.out_channels),
+                                   nn.PReLU(num_parameters=2, init=0.3))                           
+        
+        self.output_shape = conv2.output_shape
+    
+    def forward(self, x):
+        
+        x = self.dconv(x)
+        
+        return x
+
+class EncoderBlock(nn.Module):
+    def __init__(self, input_shape):
+        super().__init__()
+        
+        self.input_shape = input_shape
+        
+        dconv1 = DConv(self.input_shape, 1)
+        
+        dconv2 = DConv(dconv1.output_shape, 2)
+        
+        dconv3 = DConv(dconv2.output_shape, 3)
+        
+        self.conv1 = nn.Sequential(dconv1, dconv2, dconv3)
+        
+        conv2 = ConvWrapper(self.input_shape, 2, 3, dilation=1)
+        
+        self.conv2 = nn.Sequential(conv2,
+                                   nn.BatchNorm2d(conv2.out_channels))
+        
+        self.prelu1 = nn.PReLU(num_parameters=4, init=0.3)
+        
+        concat_shape = [self.input_shape[0], 2 * self.input_shape[1], self.input_shape[2], self.input_shape[3]]
+        
+        conv3 = ConvWrapper(concat_shape, 2, 1)
+        
+        self.conv3 = nn.Sequential(conv3, nn.BatchNorm2d(conv3.out_channels))
+        
+        self.identity = nn.Identity()
+        
+        self.prelu2 = nn.PReLU(num_parameters=2, init=0.3)
+        
+        self.output_shape = conv3.output_shape
+        
+    def forward(self, x):
+        
+        identity = self.identity(x)
+        
+        x1 = self.conv1(x)
+        
+        x2 = self.conv2(x)
+        
+        x = torch.cat([x1, x2], dim=1)
+        
+        x = self.prelu1(x)
+        
+        x = self.conv3(x)
+        
+        x = x + identity
+        
+        x = self.prelu2(x)
+        
+        return x
+        
+        
+
+class Encoder(nn.Module):
+    def __init__(self, input_shape, reduction):
+        super(Encoder, self).__init__()
+        
+        self.input_shape = input_shape
+        
+        self.input_size = np.prod(self.input_shape[1:])
+        
+        conv5 = ConvWrapper(self.input_shape, 2, 5)
+        
+        encoder_block = EncoderBlock(conv5.output_shape)
+        
+        self.encoder = nn.Sequential(conv5,
+                                     nn.BatchNorm2d(conv5.out_channels),
+                                     encoder_block)
+        
+        fc_input_shape = [self.input_shape[0], np.prod(encoder_block.output_shape[1:])]
+        
+        self.fc = LinearWrapper(fc_input_shape, self.input_size // reduction)
+        
+        self.output_shape = self.fc.output_shape
+    
+    def forward(self, x):
+        
+        x = self.encoder(x)
+        
+        x = x.view(x.size(0), -1)
+        
+        x = self.fc(x)
+        
+        return x
+
+class DecoderBlock(nn.Module):
+    def __init__(self, input_shape, expansion):
+        super().__init__()
+        
+        self.input_shape = input_shape
+        
+        self.expansion = expansion
+        
+        out_channels = 8 * expansion
+        
+        groups = 4 * expansion
+        
+        conv1 = ConvWrapper(self.input_shape, out_channels, 3, dilation=2)
+        
+        conv2 = ConvWrapper(conv1.output_shape, out_channels, (1, 3), dilation=3, groups=groups)
+        
+        conv3 = ConvWrapper(conv2.output_shape, out_channels, (3, 1), dilation=3, groups=groups)
+        
+        conv4 = ConvWrapper(conv3.output_shape, 2, 3)
+        
+        self.conv1 = nn.Sequential(conv1,
+                                   nn.BatchNorm2d(out_channels),
+                                   nn.PReLU(num_parameters=out_channels, init=0.3),
+                                   conv2,
+                                   nn.BatchNorm2d(out_channels),
+                                #    nn.ShuffleChannel(groups),
+                                   nn.PReLU(num_parameters=out_channels, init=0.3),
+                                   conv3,
+                                   nn.BatchNorm2d(out_channels),
+                                #    nn.shuffleChannel(groups),
+                                   nn.PReLU(num_parameters=out_channels, init=0.3),
+                                   conv4)
+        
+        conv1 = ConvWrapper(self.input_shape, out_channels, (1, 3))
+        
+        conv2 = ConvWrapper(conv1.output_shape, out_channels, (5, 1), groups=groups)
+        
+        conv3 = ConvWrapper(conv2.output_shape, out_channels, (1, 5), groups=groups)
+        
+        conv4 = ConvWrapper(conv3.output_shape, 2, (3, 1))
+        
+        self.conv2 = nn.Sequential(conv1,
+                                   nn.BatchNorm2d(out_channels),
+                                   nn.PReLU(num_parameters=out_channels, init=0.3),
+                                   conv2,
+                                   nn.BatchNorm2d(out_channels),
+                                #    nn.ShuffleChannel(groups),
+                                   nn.PReLU(num_parameters=out_channels, init=0.3),
+                                   conv3,
+                                   nn.BatchNorm2d(out_channels),
+                                #    nn.shuffleChannel(groups),
+                                   nn.PReLU(num_parameters=out_channels, init=0.3),
+                                   conv4)
+        
+        self.identity = nn.Identity()
+        
+        self.prelu1 = nn.PReLU(num_parameters=4, init=0.3)
+        
+        self.prelu2 = nn.PReLU(num_parameters=2, init=0.3)
+        
+        concat_shape = [self.input_shape[0], 2 * self.input_shape[1], self.input_shape[2], self.input_shape[3]]
+        
+        conv = ConvWrapper(concat_shape, 2, 1)
+        
+        self.conv3 = nn.Sequential(conv, nn.BatchNorm2d(conv.out_channels))
+                                   
+        self.output_shape = conv.output_shape
+        
+    def forward(self, x):
+        
+        identity = self.identity(x)
+        
+        x1 = self.conv1(x)
+        
+        x2 = self.conv2(x)
+        
+        x = torch.cat([x1, x2], dim=1)
+        
+        x = self.prelu1(x)
+        
+        x = self.conv3(x)
+    
+        x = x + identity
+        
+        x = self.prelu2(x)
+        
+        return x
+    
+
+class Decoder(nn.Module):
+    def __init__(self, input_shape, output_shape, expansion):
+        super(Decoder, self).__init__()
+        
+        self.input_shape = input_shape
+        
+        self.output_shape = output_shape
+        
+        self.output_size = np.prod(self.output_shape[1:])
+        
+        self.fc = LinearWrapper(self.input_shape, self.output_size)
+        
+        conv5 = ConvWrapper(self.output_shape, 2, 5)
+        
+        decoderBlock1 = DecoderBlock(conv5.output_shape, expansion)
+        
+        decoderBlock2 = DecoderBlock(decoderBlock1.output_shape, expansion)
+        
+        self.decoder = nn.Sequential(conv5,
+                                     nn.BatchNorm2d(conv5.out_channels),
+                                     nn.PReLU(num_parameters=2, init=0.3),
+                                     decoderBlock1,
+                                     decoderBlock2)
+        
+        
+        self.output_shape = decoderBlock2.output_shape
+    
+    def forward(self, x):
+        
+        x = self.fc(x)
+        
+        x = x.view(x.shape[0], *self.output_shape[1:])
+        
+        x = self.decoder(x)
+        
+        return x
+
+class DCRNet(nn.Module):
+    def __init__(self, input_shape, reduction=4, expansion=1):
+        super(DCRNet, self).__init__()
+        
+        self.input_shape = input_shape
+        
+        encoder = Encoder(self.input_shape, reduction)
+        
+        decoder = Decoder(encoder.output_shape, self.input_shape, expansion)
+        
+        self.net = nn.Sequential(encoder, decoder, nn.Sigmoid())
+        
+        # TODO: Add kaiming init
+        
+    def forward(self, x):
+        return self.net(x)
+        
+        
