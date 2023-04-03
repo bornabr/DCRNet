db173562bcfa0a952090f571bc3d89185554f3dc
Borna Barahimi <bornabarahimi@gmail.com>
2023-04-03 17:31:14 -0400

    added some fixes for the model

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from dataset import Cost2100DataLoader
 
 from torch.utils.data import DataLoader, TensorDataset
+from torch.optim.lr_scheduler import _LRScheduler
 
 import logging
 import os
@@ -40,6 +41,21 @@ def evaluator(pred, data):
         
         return nmse
 
+class WarmUpCosineAnnealingLR(_LRScheduler):
+    def __init__(self, optimizer, T_max, T_warmup, eta_min=0, last_epoch=-1):
+        self.T_max = T_max
+        self.T_warmup = T_warmup
+        self.eta_min = eta_min
+        super(WarmUpCosineAnnealingLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        if self.last_epoch < self.T_warmup:
+            return [base_lr * self.last_epoch / self.T_warmup for base_lr in self.base_lrs]
+        else:
+            k = 1 + np.cos(np.pi * (self.last_epoch - self.T_warmup) / (self.T_max - self.T_warmup))
+            return [self.eta_min + (base_lr - self.eta_min) * k / 2 for base_lr in self.base_lrs]
+
+
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg):
 	
@@ -48,8 +64,9 @@ def main(cfg):
 
 	time = datetime.now().strftime("%Y%m%d-%H%M%S")
 
-	# Set up Tensorboard
-	writer = SummaryWriter(f'{cfg.tensorboard_dir}/v{cfg.version}-{time}')
+	if not cfg.evaluate:
+		# Set up Tensorboard
+		writer = SummaryWriter(f'{cfg.tensorboard_dir}/v{cfg.version}-{time}')
 
 	# Set device
 	if cfg.gpu is None:
@@ -62,7 +79,7 @@ def main(cfg):
 	print("Using Device: {}".format(device))
 
 	# Load Model
-	model = DCRNet(cfg.db.shape).to(device)
+	model = DCRNet(cfg.db.shape, reduction=cfg.reduction, expansion=cfg.expansion).to(device)
 	
 	# Load Dataset
 	if cfg.db.name == 'small':
@@ -83,23 +100,33 @@ def main(cfg):
 	# Train Model
 	criterion = nn.MSELoss()
 	optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
-	# scheduler = torch.optim.lr_scheduler.WarmUpCosineAnnealingLR(optimizer=optimizer,
-	# 										 T_max=cfg.epochs * len(train_loader),
-	# 										 T_warmup=30 * len(train_loader),
-	# 										 eta_min=5e-5)
-	scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer,
-														   T_max=cfg.epochs * len(train_loader),
-														   eta_min=5e-5)
+	scheduler = WarmUpCosineAnnealingLR(optimizer=optimizer,
+											 T_max=cfg.epochs * len(train_loader),
+											 T_warmup= 30 * len(train_loader),
+											 eta_min=5e-5)
+	# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer,
+	# 													   T_max=cfg.epochs * len(train_loader),
+	# 													   eta_min=5e-5)
 
-	best_nmse = 1e10
-	best_epoch = 0
+	best_nmse = 10
+	best_epoch = -1
 	
+	if cfg.evaluate:
+		# Load Model
+		state = torch.load(os.path.join(cfg.checkpoints_dir, cfg.test_model_name))
+		model.load_state_dict(state['state_dict'])
+
+		# Test Model
+		test_loss, nmse = test(test_loader, model, device, criterion)
+		log.info('====> Test NMSE: {:.3e}'.format(nmse))
+		return
+
 	for epoch in range(cfg.epochs):
 		train_loss = train(cfg, writer, epoch, train_loader, model, device, criterion, optimizer, scheduler)
-		if epoch % cfg.val_interval == 0:
+		if epoch % cfg.val_interval == 0 or epoch == cfg.epochs - 1:
 			val_loss, nmse = validate(writer, epoch, val_loader, model, device, criterion)
 
-			if nmse < best_nmse:
+			if nmse <= best_nmse:
 				best_nmse = nmse
 				best_epoch = epoch
 				# Save Model
@@ -114,6 +141,8 @@ def main(cfg):
 					}
 					torch.save(state, os.path.join(cfg.checkpoints_dir, f'v{cfg.version}-{time}-best_model.pth'))	
 
+			# Log Best Epoch, Best NMSE
+			log.info('====> Best Epoch: {}\tBest NMSE: {:.3e}'.format(best_epoch, best_nmse))
 
 def train(cfg, writer, epoch, train_loader, model, device, criterion, optimizer, scheduler):
 	model.train()
@@ -128,13 +157,12 @@ def train(cfg, writer, epoch, train_loader, model, device, criterion, optimizer,
 		optimizer.step()
 		scheduler.step()
 		train_loss += loss.item()
-		if batch_idx % cfg.log_interval == 0:
+		if batch_idx % cfg.log_interval == 0 or batch_idx == num_batches - 1:
 			# Log Epoch, Batch, Loss, LR
-			log.info('Train Epoch: {} [{}/{}]\tLoss: {:.6f}\tLR: {:.6f}'
+			log.info('Train Epoch: {} [{}/{}]\tLoss: {:.3e}\tLR: {:.2e}'
          		.format(epoch, batch_idx, num_batches, loss.item(), scheduler.get_last_lr()[0]))
-
-	train_loss /= len(train_loader.dataset)
-	log.info('====> Epoch: {} Average loss: {:.6f}'.format(epoch, train_loss))
+	train_loss /= num_batches
+	log.info('====> Epoch: {} Average loss: {:.3e}'.format(epoch, train_loss))
 	writer.add_scalar('Train/Loss', train_loss, epoch)
 	writer.add_scalar('Train/LR', scheduler.get_last_lr()[0], epoch)
 	return train_loss
@@ -143,18 +171,40 @@ def validate(writer, epoch, val_loader, model, device, criterion):
 	model.eval()
 	val_loss = 0
 	nmse = 0
+	num_batches = len(val_loader)
 	with torch.no_grad():
 		for i, (data,) in enumerate(val_loader):
 			data = data.to(device)
 			output = model(data)
-			val_loss += criterion(output, data).item()
-			nmse += evaluator(output, data).item()
-	val_loss /= len(val_loader.dataset)
-	nmse /= len(val_loader.dataset)
+			val_loss += criterion(output, data)
+			nmse += evaluator(output, data)
+
+	val_loss /= num_batches
+	nmse /= num_batches
+
 	writer.add_scalar('Validation/Loss', val_loss, epoch)
 	writer.add_scalar('Validation/NMSE', nmse, epoch)
-	log.info('====> Validation set loss: {:.6f}, NMSE: {:.6f}'.format(val_loss, nmse))
+	log.info('====> Validation set loss: {:.3e}, NMSE: {:.3e}'.format(val_loss, nmse))
 	
+	return val_loss, nmse
+
+def test(val_loader, model, device, criterion):
+	model.eval()
+	val_loss = 0
+	nmse = 0
+	num_batches = len(val_loader)
+	with torch.no_grad():
+		for i, (data, _) in enumerate(val_loader):
+			data = data.to(device)
+			output = model(data)
+			val_loss += criterion(output, data)
+			nmse += evaluator(output, data)
+
+	val_loss /= num_batches
+	nmse /= num_batches
+	
+	
+	log.info('====> Test set loss: {:.3e}, NMSE: {:.3e}'.format(val_loss, nmse))
 	return val_loss, nmse
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -254,6 +254,21 @@ class DCRNet(nn.Module):
         self.net = nn.Sequential(encoder, decoder, nn.Sigmoid())
         
         # TODO: Add kaiming init
+        self.init_params()
+        
+    def init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
         
     def forward(self, x):
         return self.net(x)
